9b5dedf0cffbe1b130d97da9813c81c55fa90ce9
angrezichatterbox <gouthammohanraj@gmail.com>
2024-12-01 16:35:23 +0530

    feat:Implemented text wrapping and lines

--- ink.c
+++ ink.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <ctype.h>
 #include <termios.h>
 #include <ncurses.h>
 
 struct termios orig_termios;
 
 void die(const char *s) {
+    endwin();  
     perror(s);
     exit(1);
 }
@@ -26,103 +26,268 @@ void enableRawMode() {
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     raw.c_cc[VMIN] = 1;
     raw.c_cc[VTIME] = 1;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
-
 typedef struct {
     char *text;
     size_t length;
     size_t capacity;
+    int curr_line;
+    int curr_col;
+    int line_lengths[1000]; 
+    int total_lines;
 } TextBuffer;
 
-TextBuffer buffer = {NULL, 0, 0};
+TextBuffer buffer = {NULL, 0, 0, 0, 0, {0}, 0};
+int is_insert_mode = 0;
 
 void initTextBuffer() {
-    buffer.capacity = 1024;
+    buffer.capacity = 4096;  
     buffer.text = (char *)malloc(buffer.capacity);
     if (!buffer.text) die("malloc");
     buffer.length = 0;
+    buffer.curr_line = 0;
+    buffer.curr_col = 0;
+    buffer.total_lines = 1;
+    buffer.line_lengths[0] = 0;
 }
 
-void appendTextBuffer(char c) {
+void updateLineInfo() {
+    buffer.total_lines = 1;
+    buffer.line_lengths[0] = 0;
+    int curr_line_length = 0;
+
+    for (size_t i = 0; i < buffer.length; i++) {
+        if (buffer.text[i] == '\n') {
+            buffer.line_lengths[buffer.total_lines - 1] = curr_line_length;
+            buffer.total_lines++;
+            curr_line_length = 0;
+        } else {
+            curr_line_length++;
+        }
+    }
+    buffer.line_lengths[buffer.total_lines - 1] = curr_line_length;
+}
+
+void insertChar(char c) {
     if (buffer.length + 1 >= buffer.capacity) {
         buffer.capacity *= 2;
         buffer.text = (char *)realloc(buffer.text, buffer.capacity);
         if (!buffer.text) die("realloc");
     }
-    buffer.text[buffer.length++] = c;
+    size_t abs_pos = 0;
+    for (int i = 0; i < buffer.curr_line; i++) {
+        abs_pos += buffer.line_lengths[i] + 1;  
+    }
+    abs_pos += buffer.curr_col;
+
+    memmove(buffer.text + abs_pos + 1, 
+            buffer.text + abs_pos, 
+            buffer.length - abs_pos);
+    
+    buffer.text[abs_pos] = c;
+    buffer.length++;
+
+    if (c == '\n') {
+        buffer.total_lines++;
+        buffer.curr_line++;
+        buffer.curr_col = 0;
+    } else {
+        buffer.curr_col++;
+    }
+
+    updateLineInfo();
 }
 
-void deleteTextBuffer() {
-    if (buffer.length > 0) {
-        buffer.length--;
+void deleteChar() {
+    if (buffer.length == 0) return;
+    size_t abs_pos = 0;
+    for (int i = 0; i < buffer.curr_line; i++) {
+        abs_pos += buffer.line_lengths[i] + 1;
     }
+    abs_pos += buffer.curr_col;
+
+    if (abs_pos == 0) return;
+
+    if (buffer.curr_col == 0 && buffer.curr_line > 0) {
+        buffer.curr_line--;
+        buffer.curr_col = buffer.line_lengths[buffer.curr_line];
+    } else if (buffer.curr_col > 0) {
+        buffer.curr_col--;
+    }
+    memmove(buffer.text + abs_pos - 1, 
+            buffer.text + abs_pos, 
+            buffer.length - abs_pos);
+    
+    buffer.length--;
+
+    updateLineInfo();
 }
 
 void render() {
     clear();
-    for (size_t i = 0; i < buffer.length; i++) {
-        addch(buffer.text[i]);
+    int line_num_width = 5;  
+    for (int line = 0; line < buffer.total_lines; line++) {
+        mvprintw(line, 0, "%3d ", line + 1);
+        size_t line_start = 0;
+        for (int prev_line = 0; prev_line < line; prev_line++) {
+            line_start += buffer.line_lengths[prev_line] + 1; 
+        }
+
+        for (int col = 0; col < buffer.line_lengths[line]; col++) {
+            mvaddch(line, line_num_width + col, buffer.text[line_start + col]);
+        }
     }
+
+    move(buffer.curr_line, line_num_width + buffer.curr_col);
+    curs_set(1);
+    if (is_insert_mode) {
+        mvprintw(LINES - 1, 0, "-- INSERT MODE --");
+    } else {
+        mvprintw(LINES - 1, 0, "-- COMMAND MODE --");
+    }
+
     refresh();
+}
+
+void moveCursorUp() {
+    if (buffer.curr_line > 0) {
+        buffer.curr_line--;
+        buffer.curr_col = (buffer.curr_col > buffer.line_lengths[buffer.curr_line]) 
+                          ? buffer.line_lengths[buffer.curr_line] 
+                          : buffer.curr_col;
+    }
+}
+
+void moveCursorDown() {
+    if (buffer.curr_line < buffer.total_lines - 1) {
+        buffer.curr_line++;
+        buffer.curr_col = (buffer.curr_col > buffer.line_lengths[buffer.curr_line]) 
+                          ? buffer.line_lengths[buffer.curr_line] 
+                          : buffer.curr_col;
+    }
+}
+
+void moveCursorLeft() {
+    if (buffer.curr_col > 0) {
+        buffer.curr_col--;
+    } else if (buffer.curr_line > 0) {
+        buffer.curr_line--;
+        buffer.curr_col = buffer.line_lengths[buffer.curr_line];
+    }
+}
+
+void moveCursorRight() {
+    if (buffer.curr_col < buffer.line_lengths[buffer.curr_line]) {
+        buffer.curr_col++;
+    } else if (buffer.curr_line < buffer.total_lines - 1) {
+        buffer.curr_line++;
+        buffer.curr_col = 0;
+    }
 }
 
 void loadFile(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (!file) {
-        perror("Error opening file for reading");
+        mvprintw(LINES - 2, 0, "Error opening file: %s", filename);
+        refresh();
         return;
     }
-    char c;
+
+    buffer.length = 0;
+    buffer.curr_line = 0;
+    buffer.curr_col = 0;
+
+    int c;
     while ((c = fgetc(file)) != EOF) {
-        appendTextBuffer(c);
+        insertChar(c);
     }
+
     fclose(file);
+    updateLineInfo();
 }
 
 void saveFile(const char *filename) {
     FILE *file = fopen(filename, "w");
     if (!file) {
-        perror("Error opening file for writing");
+        mvprintw(LINES - 2, 0, "Error saving file: %s", filename);
+        refresh();
         return;
     }
+
     fwrite(buffer.text, 1, buffer.length, file);
     fclose(file);
+    mvprintw(LINES - 2, 0, "File saved successfully!");
+    refresh();
 }
 
 int main(int argc, char *argv[]) {
-    enableRawMode();
     initscr();
-    noecho();
-    cbreak();
+    raw();
     keypad(stdscr, TRUE);
-
+    noecho();
+    
+    enableRawMode();
     initTextBuffer();
-
     if (argc > 1) {
         loadFile(argv[1]);
     }
 
     int ch;
-    while ((ch = getch()) != 'q') {
-        if (ch == 127) {
-            deleteTextBuffer();
-        } else if (ch == 19) { 
-            if (argc > 1) {
-                saveFile(argv[1]);
-            } else {
-                printw("No file specified to save.\n");
-                refresh();
-            }
-        } else if (ch != 27) {
-            appendTextBuffer(ch);
-        }
+    while (1) {
         render();
-    }
+        ch = getch();
 
-    endwin();
-    free(buffer.text);
-    return 0;
+        if (is_insert_mode) {
+            switch (ch) {
+                case 27:  
+                    is_insert_mode = 0;
+                    break;
+                case KEY_BACKSPACE:
+                case 127:
+                    deleteChar();
+                    break;
+                case KEY_ENTER:
+                case 10:
+                    insertChar('\n');
+                    break;
+                default:
+                    if (ch >= 32 && ch <= 126) {  
+                        insertChar(ch);
+                    }
+                    break;
+            }
+        } else {
+            switch (ch) {
+                case 'q':
+                    endwin();
+                    free(buffer.text);
+                    return 0;
+                case 's':
+                    if (argc > 1) {
+                        saveFile(argv[1]);
+                    }
+                    break;
+                case 'i':
+                    is_insert_mode = 1;
+                    break;
+                case 'h':
+                    moveCursorLeft();
+                    break;
+                case 'j':
+                    moveCursorDown();
+                    break;
+                case 'k':
+                    moveCursorUp();
+                    break;
+                case 'l':
+                    moveCursorRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
