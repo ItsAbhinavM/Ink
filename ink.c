e651617409ba8d106d731a5ac0d2b7306defc3b2
angrezichatterbox <gouthammohanraj@gmail.com>
2024-12-01 14:18:24 +0530

    feat:Implemented a basic text using C

--- ink.c
+++ ink.c
@@ -1,51 +1,128 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <termios.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <termios.h>
+#include <ncurses.h>
 
 struct termios orig_termios;
 
 void die(const char *s) {
-  perror(s);
-  exit(1);
+    perror(s);
+    exit(1);
 }
 
 void disableRawMode() {
- if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-    die("tcsetattr");
-} // This is to enable the characters being shown in the terminal on exit as the previous functions disables it 
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
-  struct termios raw = orig_termios;
-  raw.c_oflag &= ~(OPOST); // Disables the output processing that is it disable the \n that comes behind when we press enter 
-  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Disables the ctrl-s and ctrl-q and ctrl-m
-  raw.c_cflag |= (CS8); // Sets the character size to 8 bits per byte
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // Make the terminal to read one character at a time instead of line at a time // SIG is to disable the signals like ctrl-c and ctrl-z and ctrl-v
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-  raw.c_cc[VMIN] = 0; // This is to set the minimum number of bytes of input needed before read() can return
-  raw.c_cc[VTIME] = 1; // This is to set the maximum amount of time to wait before read() returns
-} // This function is to disable the character from being shown in the terminal when typing 
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
+    raw.c_oflag &= ~(OPOST);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_cc[VMIN] = 1;
+    raw.c_cc[VTIME] = 1;
+}
 
+typedef struct {
+    char *text;
+    size_t length;
+    size_t capacity;
+} TextBuffer;
 
+TextBuffer buffer = {NULL, 0, 0};
 
-int main() {
-    enableRawMode();
-    char c ;
-    while (1) {
-    char c = '\0';
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("read");
-    read(STDIN_FILENO, &c, 1);
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
+void initTextBuffer() {
+    buffer.capacity = 1024;
+    buffer.text = (char *)malloc(buffer.capacity);
+    if (!buffer.text) die("malloc");
+    buffer.length = 0;
+}
+
+void appendTextBuffer(char c) {
+    if (buffer.length + 1 >= buffer.capacity) {
+        buffer.capacity *= 2;
+        buffer.text = (char *)realloc(buffer.text, buffer.capacity);
+        if (!buffer.text) die("realloc");
     }
-    if (c == 'q') break;
-  }
-  return 0 ;
+    buffer.text[buffer.length++] = c;
+}
+
+void deleteTextBuffer() {
+    if (buffer.length > 0) {
+        buffer.length--;
+    }
+}
+
+void render() {
+    clear();
+    for (size_t i = 0; i < buffer.length; i++) {
+        addch(buffer.text[i]);
+    }
+    refresh();
+}
+
+void loadFile(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file for reading");
+        return;
+    }
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        appendTextBuffer(c);
+    }
+    fclose(file);
+}
+
+void saveFile(const char *filename) {
+    FILE *file = fopen(filename, "w");
+    if (!file) {
+        perror("Error opening file for writing");
+        return;
+    }
+    fwrite(buffer.text, 1, buffer.length, file);
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    enableRawMode();
+    initscr();
+    noecho();
+    cbreak();
+    keypad(stdscr, TRUE);
+
+    initTextBuffer();
+
+    if (argc > 1) {
+        loadFile(argv[1]);
+    }
+
+    int ch;
+    while ((ch = getch()) != 'q') {
+        if (ch == 127) {
+            deleteTextBuffer();
+        } else if (ch == 19) { 
+            if (argc > 1) {
+                saveFile(argv[1]);
+            } else {
+                printw("No file specified to save.\n");
+                refresh();
+            }
+        } else if (ch != 27) {
+            appendTextBuffer(ch);
+        }
+        render();
+    }
+
+    endwin();
+    free(buffer.text);
+    return 0;
 }
