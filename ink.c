a2f1ce44b54ce0826c52dc6f0d60cdf947627380
Joseph T Mathew <josephtmathew@Josephs-MacBook-Air.local>
2024-12-04 01:30:00 +0530

    feat: add undo and redo

--- ink.c
+++ ink.c
@@ -46,6 +46,19 @@ typedef struct {
 TextBuffer buffer = {0};
 int is_insert_mode = 0;
 
+typedef struct {
+    char action_type; 
+    size_t position;
+    char character; 
+} Action;
+
+#define MAX_HISTORY 1000
+Action undo_stack[MAX_HISTORY];
+Action redo_stack[MAX_HISTORY];
+int undo_top = -1;
+int redo_top = -1;
+
+
 void initTextBuffer() {
     buffer.capacity = 4096;  
     buffer.text = (char *)malloc(buffer.capacity);
@@ -73,6 +86,26 @@ void updateLineInfo() {
     }
     buffer.line_lengths[buffer.total_lines - 1] = curr_line_length;
 }
+void pushUndo(Action action) {
+    if (undo_top < MAX_HISTORY - 1) {
+        undo_stack[++undo_top] = action;
+    }
+}
+
+Action popUndo() {
+    return undo_top >= 0 ? undo_stack[undo_top--] : (Action){0};
+}
+
+void pushRedo(Action action) {
+    if (redo_top < MAX_HISTORY - 1) {
+        redo_stack[++redo_top] = action;
+    }
+}
+
+Action popRedo() {
+    return redo_top >= 0 ? redo_stack[redo_top--] : (Action){0};
+}
+
 
 void insertChar(char c) {
     buffer.curr_line = buffer.cursor_row;
@@ -84,14 +117,16 @@ void insertChar(char c) {
     }
     size_t abs_pos = 0;
     for (int i = 0; i < buffer.curr_line; i++) {
-        abs_pos += buffer.line_lengths[i] + 1;  
+        abs_pos += buffer.line_lengths[i] + 1;
     }
     abs_pos += buffer.curr_col;
 
-    memmove(buffer.text + abs_pos + 1, 
-            buffer.text + abs_pos, 
-            buffer.length - abs_pos);
-    
+    // Log to undo stack
+    Action action = {'i', abs_pos, c};
+    pushUndo(action);
+    redo_top = -1; // Clear redo stack
+
+    memmove(buffer.text + abs_pos + 1, buffer.text + abs_pos, buffer.length - abs_pos);
     buffer.text[abs_pos] = c;
     buffer.length++;
 
@@ -104,16 +139,15 @@ void insertChar(char c) {
         buffer.curr_col++;
         buffer.cursor_col++;
     }
-    buffer.curr_col++;
     updateLineInfo();
 }
 
 void deleteChar() {
     buffer.curr_line = buffer.cursor_row;
     buffer.curr_col = buffer.cursor_col;
-    
+
     if (buffer.length == 0) return;
-    
+
     size_t abs_pos = 0;
     for (int i = 0; i < buffer.curr_line; i++) {
         abs_pos += buffer.line_lengths[i] + 1;
@@ -122,28 +156,112 @@ void deleteChar() {
 
     if (abs_pos == 0) return;
 
+    
+    char deleted_char = buffer.text[abs_pos - 1];
+    Action action = {'d', abs_pos - 1, deleted_char};
+    pushUndo(action);
+    redo_top = -1; 
+
     if (buffer.curr_col == 0 && buffer.curr_line > 0) {
         size_t prev_line_length = buffer.line_lengths[buffer.curr_line - 1];
-        
+
         buffer.cursor_row--;
         buffer.cursor_col = prev_line_length;
-        
-        memmove(buffer.text + abs_pos - 1, 
-                buffer.text + abs_pos, 
-                buffer.length - abs_pos);
-        
+
+        memmove(buffer.text + abs_pos - 1, buffer.text + abs_pos, buffer.length - abs_pos);
         buffer.length--;
     } else if (buffer.curr_col > 0) {
-        memmove(buffer.text + abs_pos - 1, 
-                buffer.text + abs_pos, 
-                buffer.length - abs_pos);
-        
+        memmove(buffer.text + abs_pos - 1, buffer.text + abs_pos, buffer.length - abs_pos);
         buffer.length--;
         buffer.cursor_col--;
     }
-    
+
     updateLineInfo();
 }
+
+void undo() {
+    if (undo_top < 0) return;
+
+    Action action = popUndo();
+    pushRedo(action);
+
+    size_t abs_pos = action.position;
+    if (action.action_type == 'i') {
+        
+        memmove(buffer.text + abs_pos, buffer.text + abs_pos + 1, buffer.length - abs_pos - 1);
+        buffer.length--;
+
+        
+        buffer.cursor_row = 0;
+        buffer.cursor_col = 0;
+        for (int i = 0; i < buffer.total_lines && abs_pos > buffer.line_lengths[i]; i++) {
+            abs_pos -= (buffer.line_lengths[i] + 1);
+            buffer.cursor_row++;
+        }
+        buffer.cursor_col = abs_pos;
+
+    } else if (action.action_type == 'd') {
+        
+        memmove(buffer.text + abs_pos + 1, buffer.text + abs_pos, buffer.length - abs_pos);
+        buffer.text[abs_pos] = action.character;
+        buffer.length++;
+
+        
+        buffer.cursor_row = 0;
+        buffer.cursor_col = 0;
+        for (int i = 0; i < buffer.total_lines && abs_pos >= buffer.line_lengths[i]; i++) {
+            abs_pos -= (buffer.line_lengths[i] + 1);
+            buffer.cursor_row++;
+        }
+        buffer.cursor_col = abs_pos + 1; 
+    }
+
+    updateLineInfo();
+}
+
+
+void redo() {
+    if (redo_top < 0) return;
+
+    Action action = popRedo();
+    pushUndo(action);
+
+    size_t abs_pos = action.position;
+
+    if (action.action_type == 'i') { 
+        memmove(buffer.text + abs_pos + 1, buffer.text + abs_pos, buffer.length - abs_pos);
+        buffer.text[abs_pos] = action.character;
+        buffer.length++;
+
+        
+        buffer.cursor_row = 0;
+        buffer.cursor_col = 0;
+        for (int i = 0; i < buffer.total_lines && abs_pos > buffer.line_lengths[i]; i++) {
+            abs_pos -= (buffer.line_lengths[i] + 1);
+            buffer.cursor_row++;
+        }
+        buffer.cursor_col = abs_pos;
+
+    } else if (action.action_type == 'd') { 
+        memmove(buffer.text + abs_pos, buffer.text + abs_pos + 1, buffer.length - abs_pos - 1);
+        buffer.length--;
+
+        
+        buffer.cursor_row = 0;
+        buffer.cursor_col = 0;
+        for (int i = 0; i < buffer.total_lines && abs_pos > buffer.line_lengths[i]; i++) {
+            abs_pos -= (buffer.line_lengths[i] + 1);
+            buffer.cursor_row++;
+        }
+        buffer.cursor_col = abs_pos;
+    }
+
+    updateLineInfo(); 
+}
+
+
+
+
 
 int is_visual_mode = 0;
 int selection_start_row = -1;
@@ -348,6 +466,13 @@ int main(int argc, char *argv[]) {
                 case 'l':
                     moveCursorRight();
                     break;
+                case 'u': 
+                    undo();
+                    break;
+                case 'r': 
+                    redo();
+                    break;
+
                 default:
                     break;
             }
